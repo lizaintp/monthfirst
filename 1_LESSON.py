15abefcaa2feb4781f636bebce09a0c7f6ba53bd
lizaintp <intpliza@gmail.com>
2024-02-14 17:26:00 +0600

    q

---
+++ 1_LESSON.py
@@ -0,0 +1,63 @@
+print("Hello world")
+
+# Переменные
+name = "Eliza"
+print(name)
+
+
+print(8+8)  # Сложение  
+print(20-20)  # Вычитание
+print(4*5)  # Умножение
+print(100/40)  # Деление 
+
+
+print(30/8)  # Обычное деление
+print(30//8)  # Деление с округлением вниз
+print(30%8)  # Деление по модулю
+
+
+print(3**3)  #
+
+print(9==8)
+print(9!=10)  
+print(9>5)  
+print(9<5)  
+print(9>=9)  
+print(9<=9)  
+
+number1 = int(input("Введите первое число: "))
+number2 = int(input("Введите второе число: "))
+print(number1*number2)
+
+
+intejer, string, float , bool
+
+intejer = int
+
+number = 89
+print(type(number))
+
+number = 5.5
+print (number)
+print(int(number))
+
+
+string - str
+
+number = "4"
+print(type(number))
+
+"Hello"
+'Hello'
+
+"""Hello"""
+
+
+#float - float 
+
+number = 24.25
+print(type(number))
+
+number1 = 10
+print(float())
+
